acb77f17e4c3e45a3978a7141ab4282181e1f9a4
Dev <gaston.siffert@gmail.com>
2024-03-14 16:34:02 +0100

    Add infra layer for the disk management

--- internal/disk/disk.go
+++ internal/disk/disk.go
@@ -1,1 +1,31 @@
+// Package disk is part of the infrastructure layer and it implements the service.Disk interface using a local directory.
 package disk
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
+)
+
+// Client of the disk package.
+type Client struct {
+	basePath string
+}
+
+// New instantiates a new Client.
+func New(basePath string) *Client {
+	return &Client{basePath: basePath}
+}
+
+// NewPageWriter creates a new file for the given name.
+func (c *Client) NewPageWriter(_ context.Context, name string) (io.WriteCloser, error) {
+	fileName := fmt.Sprintf("%s.html", name)
+	filePath := path.Join(c.basePath, fileName)
+	file, err := os.Create(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+	return file, nil
+}
--- internal/disk/disk_test.go
+++ internal/disk/disk_test.go
@@ -1,1 +1,39 @@
 package disk
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewPageWriter(t *testing.T) {
+	t.Parallel()
+
+	temporyDir := os.TempDir()
+	client := New(temporyDir)
+	expectedContent := "Hello World"
+
+	t.Run("write to file", func(t *testing.T) {
+		writer, err := client.NewPageWriter(context.Background(), "www.google.com")
+		require.NoError(t, err)
+		_, err = fmt.Fprint(writer, expectedContent)
+		require.NoError(t, err)
+		err = writer.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("read content", func(t *testing.T) {
+		expectedFile := filepath.Join(temporyDir, "www.google.com.html")
+		file, err := os.Open(expectedFile)
+		require.NoError(t, err)
+		content, err := io.ReadAll(file)
+		require.NoError(t, err)
+		assert.Equal(t, expectedContent, string(content))
+	})
+}
