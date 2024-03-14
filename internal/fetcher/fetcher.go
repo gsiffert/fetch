277657799e83f75411c9f4d7d650b379d19acedc
Dev <gaston.siffert@gmail.com>
2024-03-14 16:33:41 +0100

    Add the infra layer for downloading pages

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -1,1 +1,68 @@
+// Package fetcher is part of the infrastructure layer and it implements the service.Fetcher interface using a http.Client.
 package fetcher
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/eapache/go-resiliency/retrier"
+	"github.com/gsiffert/fetch/internal/domain"
+	"github.com/gsiffert/fetch/internal/service"
+)
+
+const (
+	maxRetries        = 5
+	initialRetryDelay = 100 * time.Millisecond
+	htmlContentType   = "text/html"
+)
+
+var retryErr = errors.New("retry")
+
+// Client to Fetch webpages.
+type Client struct {
+	httpClient *http.Client
+}
+
+// New returns a new Client.
+func New(httpClient *http.Client) *Client {
+	return &Client{httpClient: httpClient}
+}
+
+// Fetch queries the page from the given site and returns a service.FetchedItem.
+// It retries on network errors and server errors.
+func (c *Client) Fetch(ctx context.Context, site string) (*service.FetchedItem, error) {
+	r := retrier.New(retrier.ExponentialBackoff(maxRetries, initialRetryDelay), retrier.WhitelistClassifier{retryErr})
+
+	var fetchedItem *service.FetchedItem
+	err := r.Run(func() error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, site, nil)
+		if err != nil {
+			return fmt.Errorf("new request: %w", err)
+		}
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return errors.Join(fmt.Errorf("do request: %w", err), retryErr)
+		}
+
+		switch {
+		case resp.StatusCode >= http.StatusInternalServerError:
+			return errors.Join(fmt.Errorf("unexpected server error: %d", resp.StatusCode), retryErr)
+		case resp.StatusCode != http.StatusOK:
+			return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		case !strings.Contains(resp.Header.Get("Content-Type"), htmlContentType):
+			return fmt.Errorf("unexpected content type: %s", resp.Header.Get("Content-Type"))
+		default:
+		}
+
+		page := domain.NewPage(req.URL)
+		fetchedItem = &service.FetchedItem{Page: page, Content: resp.Body}
+		return nil
+	})
+
+	return fetchedItem, err
+}
--- internal/fetcher/fetcher_test.go
+++ internal/fetcher/fetcher_test.go
@@ -1,1 +1,114 @@
 package fetcher
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gsiffert/fetch/internal/domain"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const htmlContent = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test</title>
+	</head>
+	<body>
+		<h1>Test</h1>
+	</body>
+</html>
+`
+
+type retryInternalErrorServer struct {
+	count int
+}
+
+func (s *retryInternalErrorServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.count++
+	if s.count < maxRetries-1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", htmlContentType)
+	_, _ = w.Write([]byte(htmlContent))
+}
+
+func TestClient_Fetch(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name       string
+		server     http.Handler
+		assertErr  assert.ErrorAssertionFunc
+		expectResp bool
+	}{
+		{
+			name: "invalid content type",
+			server: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Header().Set("Content-Type", "application/json")
+			}),
+			assertErr: assert.Error,
+		},
+		{
+			name: "failed on 4xx",
+			server: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusNotFound)
+			}),
+			assertErr: assert.Error,
+		},
+		{
+			name: "failed on 5xx",
+			server: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+			}),
+			assertErr: assert.Error,
+		},
+		{
+			name: "success",
+			server: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Header().Set("Content-Type", htmlContentType)
+				_, _ = w.Write([]byte(htmlContent))
+			}),
+			assertErr:  assert.NoError,
+			expectResp: true,
+		},
+		{
+			name:       "success after few retries",
+			server:     &retryInternalErrorServer{},
+			assertErr:  assert.NoError,
+			expectResp: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			srv := httptest.NewServer(test.server)
+			defer srv.Close()
+
+			client := New(http.DefaultClient)
+			item, err := client.Fetch(ctx, srv.URL)
+			test.assertErr(t, err)
+			if test.expectResp {
+				withoutProtocol := strings.TrimPrefix(srv.URL, "http://")
+				assert.Equal(t, domain.PageID(srv.URL), item.Page.ID)
+				assert.Equal(t, withoutProtocol, item.Page.Site)
+				assert.Equal(t, withoutProtocol, item.Page.FileLocation)
+				b, err := io.ReadAll(item.Content)
+				require.NoError(t, err)
+				assert.Equal(t, htmlContent, string(b))
+			}
+		})
+	}
+}
